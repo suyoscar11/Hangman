1ae8fd9d274f09d2eb97fb7f8cf87e35ed4d74c8
Suyoscar11 <suyoscar11@gmail.com>
2024-08-24 00:50:21 -0600

    dummy_python_project

---
+++ hangman.py
@@ -0,0 +1,105 @@
+
+
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+
+lives = 6
+
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+        
+
+
+    if guess not in chosen_word:
+        lives= lives-1
+        print(stages[lives])
+        if lives==0:
+            print("Game Over")
+            end_of_game= True
+        if "_" not in display:
+            print("You Win")
+            end_of_game= True
+            
+        
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+  
